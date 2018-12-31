ffb2d417d1130ed4d488c7efa283bb7d8848ee2d
Michael <chaphlux@yahoo.com>
2018-12-31 01:45:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function takeANumber(line, name) {
-  for (var i = 0; i<line.length; i++){
+  /*for (var i = 0; i<line.length; i++){
 var welcome = `Welcome, ${name[i]}. You are number ${line+1} in line.`;
 }
 return welcome
+}
+*/
+console.log (${name[i]}. You are number ${line+1} in line.`)
 }
