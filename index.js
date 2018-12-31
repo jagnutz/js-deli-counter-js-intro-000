02d6e05dd042a6d5aec308378e09a6f617af986b
Michael <chaphlux@yahoo.com>
2018-12-31 02:33:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(katzDeliLine){
   var i = 0;
 for (i=0; i<=katzDeliLine.length; i++){
   if (i>=0){
-  return "The line is currently: katzDeliLine[i]"}
+  return "The line is currently: katzDeliLine[]"}
   else {
     return "The line is currently empty."
   }
