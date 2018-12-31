271c88cdce2af937d76e27de5f25c5b836d36993
Michael <chaphlux@yahoo.com>
2018-12-31 02:09:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
