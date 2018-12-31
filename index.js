7e95d80d199466e0698ebf5da562762b897a4b7a
Michael <chaphlux@yahoo.com>
2018-12-31 02:16:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  
+  if (katzDeliLine.length>0){
+    return katzDeliLine.shift()
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
 }
