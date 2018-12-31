0e70e52cea06a1d66d2e934fd5448fc8e6806c08
Michael <chaphlux@yahoo.com>
2018-12-31 01:58:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function takeANumber(line, name) {
 var i=0
-return `Welcome, ${name}. You are number ${line+1} in line.`
+return `Welcome, ${name}. You are number ${line.length+1} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  
 }
