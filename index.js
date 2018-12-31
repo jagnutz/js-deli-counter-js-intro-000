09903c626a4748aa0f20c7c83d5d0646dd5b6dd8
Michael <chaphlux@yahoo.com>
2018-12-31 01:46:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 function takeANumber(line, name) {
-  /*for (var i = 0; i<line.length; i++){
-var welcome = `Welcome, ${name[i]}. You are number ${line+1} in line.`;
-}
-return welcome
-}
-*/
 return ${name[i]}. You are number ${line+1} in line.`
 }
